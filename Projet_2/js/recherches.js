51510da1e930622e047884eb472c6c4bc8213163
Keyvan Firmin <keyvanboom334@gmail.com>
2020-11-03 16:28:48 -0400

    Barre de recherche fonctionnelle( ajout d'un fichier js )

---
+++ Projet_2/js/recherches.js
@@ -0,0 +1,50 @@
+// Mots-clés de la barre de recherche//
+function ouvrirPage()
+{
+  var a = document.getElementById("search").value;
+  if (a === "")
+  {
+    alert("Veuillez remplir 🙈");
+  }
+     
+  if (a === "cable hdmi")
+  {
+		window.open("cable_hdmi.html");
+  }
+  if (a === "casque")
+  {
+		window.open("casque_logitech.html");
+  }
+  if (a === "clavier")
+  {
+    window.open("clavier_rk918.html");
+  }
+  if (a === "mac")
+  {
+		window.open("macbook.html");
+  }
+  if (a === "tapis de souris")
+  {
+		window.open("mousepad.html");
+  }
+  if (a === "ps4")
+  {
+		window.open("ps4_pro.html");
+  }
+  if (a === "razer")
+  {
+		window.open("razer_raptor.html");
+  }
+  if (a === "souris")
+  {
+		window.open("souris_alienware.html");
+  }
+  if (a === "nintendo")
+  {
+		window.open("switch.html");
+  }
+  if (a === "xbox")
+  {
+		window.open("xbox_one_s.html");
+  }
+}
