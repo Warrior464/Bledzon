5765d2388c932a6a288c51f1cc09eb31b2cba20b
Keyvan Firmin <keyvanboom334@gmail.com>
2020-11-12 22:45:58 -0400

    Ajout de la page hors service du panier

--- Projet_2/js/recherches.js
+++ Projet_2/js/recherches.js
@@ -1,4 +1,4 @@
-// Mots-clés de la barre de recherche//
+// Mots-clés de la barre de recherche
 function ouvrirPage()
 {
   var a = document.getElementById("search").value;
@@ -6,43 +6,78 @@ function ouvrirPage()
   {
     alert("Veuillez remplir 🙈");
   }
-     
+
+  // Mots clés cable hdmi
   if (a === "cable hdmi")
   {
 		window.open("cable_hdmi.html");
   }
+  if (a === "hdmi")
+  {
+		window.open("cable_hdmi.html");
+  }
+
+  // Mots clés casque logitech
   if (a === "casque")
   {
 		window.open("casque_logitech.html");
   }
+
+  // Mots clés clavier
   if (a === "clavier")
   {
     window.open("clavier_rk918.html");
   }
+
+   // Mots clés macbook
   if (a === "mac")
   {
 		window.open("macbook.html");
   }
+  if (a === "macbook")
+  {
+		window.open("macbook.html");
+  }
+
+   // Mots clés tapis de souris
   if (a === "tapis de souris")
   {
 		window.open("mousepad.html");
   }
+  if (a === "tapis")
+  {
+		window.open("mousepad.html");
+  }
+
+   // Mots clés ps4
   if (a === "ps4")
   {
 		window.open("ps4_pro.html");
   }
+
+   // Mots clés
   if (a === "razer")
   {
 		window.open("razer_raptor.html");
   }
+
+   // Mots clés souris
   if (a === "souris")
   {
 		window.open("souris_alienware.html");
   }
+
+   // Mots clés nintendo
   if (a === "nintendo")
   {
 		window.open("switch.html");
   }
+  if (a === "switch")
+  {
+		window.open("switch.html");
+  }
+
+   // Mots clés xbox  
   if (a === "xbox")
   {
 		window.open("xbox_one_s.html");
